1316fa021df80b14d901e55f2a2e85509aea5066
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-22 21:24:02 -0500

    Update red_neuronal.py

--- red_neuronal.py
+++ red_neuronal.py
@@ -2,3 +2,7 @@ import tensorflow as tf
 from tensorflow.keras import layers, models
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
+
+
+# Cargar y preparar datos de MNIST
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
