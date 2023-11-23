3ca58ebbfb72c146c6fc39b2e326ac478a49e503
Jhon Zambrano <jhonmacias1999@gmail.com>
2023-11-22 21:24:20 -0500

    Update red_neuronal.py

--- red_neuronal.py
+++ red_neuronal.py
@@ -7,3 +7,4 @@ from tensorflow.keras.utils import to_categorical
 # Cargar y preparar datos de MNIST
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 x_train, x_test = x_train / 255.0, x_test / 255.0  # Normalizar píxeles a valores entre 0 y 1
+y_train, y_test = to_categorical(y_train), to_categorical(y_test)  # Convertir etiquetas a one-hot encoding
